cb5072ec0850a24003c32d601f81b1e17c00e660
Juan Pablo <juanpa_roto@hotmail.com>
2023-02-24 10:01:26 -0600

    ended project book page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["/index.html", "/public/*.{html,js}"],
+  content: ["./index.html", "/public/*.{html,js}"],
   theme: {
     extend: {},
   },
